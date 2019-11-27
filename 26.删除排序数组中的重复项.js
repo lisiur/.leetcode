b2cf632a079f7260fe9388e9b896b5fbeca0e051
lisiurday <lisiurday@gmail.com>
2019-11-27 13:00:51 +0800

    update

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -10,7 +10,23 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+  if (nums.length <= 1) {
+    return nums.length
+  }
+  var p = 0, q = p + 1;
+  while(q < nums.length) {
+    if (nums[q] !== nums[p]) {
+      p += 1;
+      nums[p] = nums[q];
+    }
+    q += 1;
+  }
+  nums.length = p + 1;
+  return nums.length;
 };
 // @lc code=end
+
+// var nums = [0,0,1,1,1,2,2,3,3,3];
+// removeDuplicates(nums);
+// console.log(nums);
 
